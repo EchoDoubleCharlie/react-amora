d3cae0beba006c62b957bca4edbdf752fa9f2c97
EchoDoubleCharlie <amoraalyashier00@gmail.com>
2025-01-25 13:05:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import styles from "./App.module.css";
+import { Hero } from "./components/Hero/Hero";
+
+
+
+function App() {
+  return (
+    <div className={styles.App}>
+      
+      <Hero />
+   
+    </div>
+  );
+}
+
+export default App;
